6286d233ab6a3cc2ed2e1e962f91e472e4e53292
Wantonius <hentonen@gmail.com>
2020-05-06 13:41:23 +0300

    mobx intro

---
+++ mobx/shoppingmobx/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ShoppingForm from './ShoppingForm';
+import ShoppingList from './ShoppingList';
+
+function App() {
+  return (
+    <div className="App">
+		<ShoppingForm/>
+		<hr/>
+		<ShoppingList/>
+    </div>
+  );
+}
+
+export default App;
