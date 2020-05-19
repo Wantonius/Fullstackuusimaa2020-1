4f65244501319cb70a988957b8a8d6fd33261606
Wantonius <hentonen@gmail.com>
2020-05-19 09:59:30 +0300

    vue 1

---
+++ vue/helloworld/src/components/MyFirstComponent.vue
@@ -0,0 +1,24 @@
+<template>
+	<div>
+		<p>Hello, {{firstname}}</p>
+		<p>{{computedHello}}</p>
+		<p>{{methodHello()}}</p>
+	</div>
+</template>
+
+<script>
+export default {
+	name:"MyFirstComponent",
+	props:['firstname'],
+	computed: {
+		computedHello: function() {
+			return "Hello from computed value, "+this.firstname
+		}
+	},
+	methods: {
+		methodHello: function() {
+			return "Hello from method value, "+this.firstname
+		}
+	}
+}
+</script>
