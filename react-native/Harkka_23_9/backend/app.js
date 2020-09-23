faf09522b7f6485f42fb82d97c433d8496135e56
Erno Hentonen <erno.hentonen@opiframe.com>
2020-09-23 12:56:30 +0300

    test

--- react-native/Harkka_23_9/backend/app.js
+++ react-native/Harkka_23_9/backend/app.js
@@ -17,7 +17,7 @@ let id = 100;
 enableCors = (req,res,next) => {
 	res.header('Access-Control-Allow-Origin','*');
 	res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
-	res.header('Access-Control-Allow-Headers','Content-type, token');
+	res.header('Access-Control-Allow-Headers','Content-type, token,messagetoken');
 	
 	return next();
 }
